95ac59e4f997e9a946f6b9f41724d1a0144b615b
renan-augusto <renan_acm1992@hotmail.com>
2023-04-03 20:54:38 -0300

    :sparkles: feat: criado o arquivo de models, models movidos para o respectivo arquivo.

---
+++ models.py
@@ -0,0 +1,20 @@
+from spe import db
+class ALUNO(db.Model):
+    IDALUNO = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    NOME = db.Column(db.String(100), nullable = False)
+    CPF = db.Column(db.String(11), unique = True )
+    DATANASCIMENTO = db.Column(db.String(10), nullable = False) #confirmar
+    SEXO = db.Column(db.String(1), nullable = False), #confirmar
+    EMAIL = db.Column(db.String(50), unique = True)
+    SEMESTRE = db.Column(db.String(1), nullable = False), 
+    RA = db.Column(db.String(8), unique = True, nullable = False)
+    def __repr__(self) :
+        return '<Name %r>' % self.name 
+
+class PROFESSOR(db.Model):
+    IDPROFESSOR = db.Column(db.Integer, primary_key = True, autoincrement = True),
+    NOME = db.Column(db.String(100), nullable = False),
+    SEXO = db.Column(db.String(1), nullable = False), #confirmar
+    CPF = db.Column(db.String(11), nullable = False)
+    def __repr__(self):
+        return '<Name % r>' % self.name
--- spe.py
+++ spe.py
@@ -14,28 +14,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = \
     )
 
 db = SQLAlchemy(app) 
-
-class ALUNO(db.Model):
-    IDALUNO = db.Column(db.Integer, primary_key = True, autoincrement = True)
-    NOME = db.Column(db.String(100), nullable = False)
-    CPF = db.Column(db.String(11), unique = True )
-    DATANASCIMENTO = db.Column(db.String(10), nullable = False) #confirmar
-    SEXO = db.Column(db.String(1), nullable = False), #confirmar
-    EMAIL = db.Column(db.String(50), unique = True)
-    SEMESTRE = db.Column(db.String(1), nullable = False), 
-    RA = db.Column(db.String(8), unique = True, nullable = False)
-    def __repr__(self) :
-        return '<Name %r>' % self.name 
-
-class PROFESSOR(db.Model):
-    IDPROFESSOR = db.Column(db.Integer, primary_key = True, autoincrement = True),
-    NOME = db.Column(db.String(100), nullable = False),
-    SEXO = db.Column(db.String(1), nullable = False), #confirmar
-    CPF = db.Column(db.String(11), nullable = False)
-    def __repr__(self):
-        return '<Name % r>' % self.name
-    
-    
+   
 @app.route('/')
 def index():
     return render_template('index.html')
