92e69b74ab0a764feed3476b93eebddddf977ce7
renan-augusto <renan_acm1992@hotmail.com>
2023-04-06 06:25:56 -0300

    :construction: preparando futuras rotas nas views

--- views.py
+++ views.py
@@ -1,7 +1,19 @@
-from flask import render_template, request, redirectm, session, flash, url_for
+from flask import render_template, request, redirect, session, flash, url_for
 from spe import app, db
 from models import ALUNO, PROFESSOR
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/login')
+def login():
+    pass
+
+@app.route('/authenticate', methods=['POST', ])
+def authenticate():
+    pass
+
+@app.route('/logout')
+def logout():
+    pass
