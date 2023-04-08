962f51d91d4d252c1f01de513fbddc059e5df517
renan-augusto <renan_acm1992@hotmail.com>
2023-04-08 17:10:01 -0300

    :construction: iniciando a construção duma lógica de login, adicionado um template de registro não estilizado

--- models.py
+++ models.py
@@ -1,22 +1,29 @@
 from spe import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class ALUNO(db.Model):
     IDALUNO = db.Column(db.Integer, primary_key = True, autoincrement = True)
     NOME = db.Column(db.String(100), nullable = False)
-    EMAIL = db.Column(db.String(50), unique = True)
+    EMAIL = db.Column(db.String(50), unique = True, nullable = False)
     RA = db.Column(db.String(8), unique = True, nullable = False)
-    # CPF = db.Column(db.String(11), unique = True )
-    # DATANASCIMENTO = db.Column(db.String(10), nullable = False) #confirmar
-    # SEXO = db.Column(db.String(1), nullable = False), #confirmar
-    # SEMESTRE = db.Column(db.String(1), nullable = False),
-
+    SENHA = db.Column(db.String(128), nullable = False)
     def __repr__(self) :
         return '<Name %r>' % self.name 
-
+    def __init__(self, NOME, EMAIL, RA, SENHA):
+        self.NOME = NOME 
+        self.EMAIL = EMAIL
+        self.RA = RA
+        self.SENHA = generate_password_hash(SENHA)
+        
+    def verify_password(self, pwd):
+        return check_password_hash(self.SENHA, pwd)
+    
+    
 class PROFESSOR(db.Model):
     IDPROFESSOR = db.Column(db.Integer, primary_key = True, autoincrement = True),
     NOME = db.Column(db.String(100), nullable = False),
-    SEXO = db.Column(db.String(1), nullable = False), #confirmar
-    CPF = db.Column(db.String(11), nullable = False)
+    EMAIL = db.Column(db.String(50), unique = True, nullable = False)
+    SENHA = db.Column(db.String(128), nullable = False)
     def __repr__(self):
         return '<Name % r>' % self.name
+    
--- views.py
+++ views.py
@@ -2,9 +2,23 @@ from flask import render_template, request, redirect, session, flash, url_for
 from spe import app, db
 from models import ALUNO, PROFESSOR
 
-@app.route('/')
+@app.route('/', methods=['POST', ])
 def index():
     return render_template('index.html')
+
+@app.route('/register', methods=['POST', ])
+def register_student():
+    if request.method == "POST":
+        name = request.form['name']
+        email = request.form['email']
+        ra = request.form['ra'] 
+        pwd = request.form['pwd']
+        
+        student = ALUNO(name, email, ra, pwd)
+        db.session.add(student)
+        db.session.commit()
+     
+    return render_template('register-student.html')
 
 @app.route('/login')
 def login():
